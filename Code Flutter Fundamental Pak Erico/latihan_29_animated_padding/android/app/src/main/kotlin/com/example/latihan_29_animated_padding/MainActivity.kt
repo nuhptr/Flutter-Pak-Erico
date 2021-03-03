386d534193e0c7e47a51ac9fe323c75c9828233d
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-03-04 04:32:46 +0700

    animated padding

---
+++ Code Flutter Fundamental Pak Erico/latihan_29_animated_padding/android/app/src/main/kotlin/com/example/latihan_29_animated_padding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_29_animated_padding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Code Flutter Fundamental Pak Erico/latihan_29_animated_padding/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animated Padding"),
+      ),
+    );
+  }
+}
