c2698c53c47db375272a758f883d20f6e87b7aa5
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-03-04 06:26:40 +0700

    fix animated padding

--- Code Flutter Fundamental Pak Erico/latihan_29_animated_padding/lib/main.dart
+++ Code Flutter Fundamental Pak Erico/latihan_29_animated_padding/lib/main.dart
@@ -24,11 +24,78 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var padding = 5.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Animated Padding"),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            padding = 20;
+          });
+        },
+        child: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(padding),
+                      child: Container(
+                        color: Colors.red,
+                      ),
+                    ),
+                    flex: 1,
+                  ),
+                  Flexible(
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(padding),
+                      child: Container(
+                        color: Colors.green,
+                      ),
+                    ),
+                    flex: 1,
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(padding),
+                      child: Container(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    flex: 1,
+                  ),
+                  Flexible(
+                    child: AnimatedPadding(
+                      duration: Duration(seconds: 1),
+                      padding: EdgeInsets.all(padding),
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    flex: 1,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
