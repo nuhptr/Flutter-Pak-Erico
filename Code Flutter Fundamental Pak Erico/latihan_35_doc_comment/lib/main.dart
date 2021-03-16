5f6d846479243a4e2a02bce41ac96a5462ede3ea
nuhptr <nugrahaadi733@gmail.com>
2021-03-16 14:00:45 +0700

    comment

--- Code Flutter Fundamental Pak Erico/latihan_35_doc_comment/lib/main.dart
+++ Code Flutter Fundamental Pak Erico/latihan_35_doc_comment/lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final UserProfile userProfile = UserProfile();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- Code Flutter Fundamental Pak Erico/latihan_35_doc_comment/lib/user_profile.dart
+++ Code Flutter Fundamental Pak Erico/latihan_35_doc_comment/lib/user_profile.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+///? Class to Show Profile User
+//
 class UserProfile extends StatelessWidget {
+  ///? field untuk menyimpan nama user
   final String name;
+
+  ///? field untuk menyimpan role user
   final String role;
+
+  ///? field untuk menyimpan foto user
   final String photoUrl;
 
+  /// * [name] default = `no name`.
+  /// * [role] default = `no role`.
+  /// * [photoUrl] berisi **foto user** = `null`
   const UserProfile(
       {this.name = 'no name', this.role = 'No Role', this.photoUrl});
 
@@ -14,29 +24,31 @@ class UserProfile extends StatelessWidget {
       appBar: AppBar(
         title: Text('Doc Comment'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image(
-            image: NetworkImage((photoUrl != null)
-                ? photoUrl
-                : "https://cdn4.iconFinder.com/data.icons/web-ui-color/128/Account-512.png"),
-            fit: BoxFit.cover,
-            width: 200,
-            height: 200,
-          ),
-          SizedBox(height: 10),
-          Text(
-            name,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "[" + role + "]",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              image: NetworkImage((photoUrl != null)
+                  ? photoUrl
+                  : "https://cdn4.iconFinder.com/data/icons/web-ui-color/128/Account-512.png"),
+              fit: BoxFit.cover,
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 10),
+            Text(
+              name,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(height: 5),
+            Text(
+              "[" + role + "]",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
