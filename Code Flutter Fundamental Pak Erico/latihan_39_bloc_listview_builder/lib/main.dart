9ddb17c7ac55205dc6337d50cf758a8d3775eb16
nuhptr <nugrahaadi733@gmail.com>
2021-04-05 11:06:07 +0700

    update

--- Code Flutter Fundamental Pak Erico/latihan_39_bloc_listview_builder/lib/main.dart
+++ Code Flutter Fundamental Pak Erico/latihan_39_bloc_listview_builder/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:latihan_39_bloc_listview_builder/bloc/product_bloc.dart';
+import 'package:latihan_39_bloc_listview_builder/model/product.dart';
 
 import 'page/main_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -14,7 +17,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      debugShowCheckedModeBanner: false,
+      home: BlocProvider(
+        create: (context) => ProductBloc([]),
+        child: MainPage(),
+      ),
     );
   }
 }
--- Code Flutter Fundamental Pak Erico/latihan_39_bloc_listview_builder/lib/page/main_page.dart
+++ Code Flutter Fundamental Pak Erico/latihan_39_bloc_listview_builder/lib/page/main_page.dart
@@ -1,24 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class MainPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:latihan_39_bloc_listview_builder/bloc/product_bloc.dart';
+import 'package:latihan_39_bloc_listview_builder/model/product.dart';
+import 'package:latihan_39_bloc_listview_builder/widget/product_card.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final Random r = Random();
+
   @override
   Widget build(BuildContext context) {
+    ProductBloc bloc = BlocProvider.of<ProductBloc>(context);
+
     return Scaffold(
       appBar: AppBar(title: Text("ListView Builder Bloc")),
-      body: Column(
+      body: ListView(
         children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Tambah'),
+          Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  bloc.add(r.nextInt(4) + 2);
+                },
+                child: Text('Tambah'),
+              ),
+              BlocBuilder<ProductBloc, List<Product>>(
+                builder: (context, products) => Expanded(
+                  child: ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      return ProductCard(
+                        imageUrl: products[index].imageUrl,
+                        name: products[index].name,
+                        price: products[index].price.toString(),
+                        onAddCartTap: () {},
+                        onIncTap: () {},
+                        onDecTap: () {},
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return Text(index.toString());
-              },
-            ),
-          )
         ],
       ),
     );
