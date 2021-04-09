76df921459a8454235dfd2a160f89afc22c8b2dd
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-04-09 15:05:13 +0700

    extension

---
+++ Code Flutter Fundamental Pak Erico/latihan_47_extension/android/app/src/main/kotlin/com/example/latihan_47_extension/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_47_extension
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Code Flutter Fundamental Pak Erico/latihan_47_extension/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final double x = -10;
+  final List<int> myList = [1, 2, 3, 4];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Extension'), centerTitle: true),
+      body: Center(
+        child: Text(
+          "Bilangan => " & myList.midElement.toString(),
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
+
+extension NumberExtension<T extends num> on num {
+  T negate() => -1 * this;
+}
+
+extension StringExtension on String {
+  String operator &(String other) => this + " " + other;
+}
+
+extension ListExtension<T> on List {
+  T get midElement =>
+      (this.length == 0) ? null : this[(this.length / 2).floor()];
+}
