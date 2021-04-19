dc735268c6580357d169b52cb54d8a8c979def3d
nuhptr <nugrahaadi733@gmail.com>
2021-04-19 20:00:27 +0700

    update test

--- Code Flutter Fundamental Pak Erico/latihan_59_bloc_test2/test/widget_test.dart
+++ Code Flutter Fundamental Pak Erico/latihan_59_bloc_test2/test/widget_test.dart
@@ -1,4 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 
-void main() {}
+import '../lib/bloc/product_bloc.dart';
+
+void main() {
+  group("testing initial Bloc", () {
+    blocTest("Inisialisasi Product Bloc",
+        build: () => ProductBloc(), expect: []);
+  });
+}
