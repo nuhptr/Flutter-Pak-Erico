e8414724ca55b4c7dbd2b4204fdb2a33adabb4ea
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-06-14 22:10:07 +0700

    flexible layout

---
+++ latihan4_flexible_layout/android/app/src/main/kotlin/com/example/latihan4_flexible_layout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan4_flexible_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan4_flexible_layout/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flexible Layout"),
+      ),
+      body: Column(
+        children: [
+          // TODO: Tidak memaksa childnya untuk ikut mengembang
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                    margin: EdgeInsets.all(5),
+                  ),
+                )
+              ],
+            ),
+          ),
+          // TODO: flexible kedua
+          Flexible(
+            // TODO: flex mempengaruhi ukuran
+            flex: 2,
+            child: Container(
+              color: Colors.green,
+              margin: EdgeInsets.all(5),
+            ),
+          ),
+          // TODO: flexible ketiga
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+              margin: EdgeInsets.all(5),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
