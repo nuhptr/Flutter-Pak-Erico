ff8dc22b936642f99d037a67b3ef27b5993dc144
nuhptr <nugrahaadi733@gmail.com>
2021-06-15 21:56:49 +0700

    inkwell

---
+++ latihan13_inkwell/android/app/src/main/kotlin/com/example/latihan13_inkwell/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan13_inkwell
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan13_inkwell/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // TODO: lock device orientation
+  WidgetsFlutterBinding().ensureVisualUpdate();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Elevated Button"),
+            ),
+            Material(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+              elevation: 2,
+              child: Container(
+                width: 100,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                  gradient: LinearGradient(
+                    colors: [Colors.green, Colors.yellow],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                ),
+                child: Material(
+                  borderRadius: BorderRadius.all(Radius.circular(4)),
+                  color: Colors.transparent,
+                  child: Center(
+                    // TODO: widget inkwell -> button yang bisa di custom
+                    child: InkWell(
+                        splashColor: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        onTap: () {},
+                        child: Text("Inkwell")),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
