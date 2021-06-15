450087c63a6c706d0fcfd052940a51635172eab2
nuhptr <nugrahaadi733@gmail.com>
2021-06-15 16:06:31 +0700

    mediaquery

---
+++ latihan12_mediaquery/android/app/src/main/kotlin/com/example/latihan12_mediaquery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan12_mediaquery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan12_mediaquery/lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        // TODO: ternary untuk posisi hp ketika digunakan
+        body: (MediaQuery.of(context).orientation == Orientation.portrait)
+            ? BuildContainer()
+            : BuildContainer2(),
+      ),
+    );
+  }
+}
+
+// TODO: ketika dalam keadaan potrait
+class BuildContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.green[300],
+              ),
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.red[300],
+              ),
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.blue[300],
+              ),
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// TODO: ketika dalam keadaan landscape
+class BuildContainer2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.green[300],
+              ),
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.red[300],
+              ),
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.blue[300],
+              ),
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
