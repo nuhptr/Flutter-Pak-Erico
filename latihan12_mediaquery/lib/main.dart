92295568076fd3dc45e3629652e7389ccca40fa0
nuhptr <nugrahaadi733@gmail.com>
2021-06-15 16:52:34 +0700

    update lock device

--- latihan12_mediaquery/lib/main.dart
+++ latihan12_mediaquery/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // TODO: lock device orientation
+  WidgetsFlutterBinding().ensureVisualUpdate();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) => runApp(MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   @override
