4ae217e86affa92b18945b967e11251af8f222d6
nuhptr <nugrahaadi733@gmail.com>
2021-06-15 15:25:00 +0700

    spacer

---
+++ latihan11_spacer/android/app/src/main/kotlin/com/example/latihan11_spacer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan11_spacer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan11_spacer/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+            child: Row(
+          children: [
+            // TODO: widget Spacer untuk memberi space -> default flex : 1
+            Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.red[300],
+              ),
+              width: 80,
+              height: 80,
+            ),
+            Spacer(),
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.blue[300],
+              ),
+            ),
+            Spacer(),
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.green[300],
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+            Spacer(),
+          ],
+        )),
+      ),
+    );
+  }
+}
