6d4f47d7c827770b8381bc151b733d5e3c2d9a23
nuhptr <nugrahaadi733@gmail.com>
2021-06-15 14:57:46 +0700

    card widget

---
+++ latihan10_card/android/app/src/main/kotlin/com/example/latihan10_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan10_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan10_card/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: ListView(
+            children: [
+              buildCard(Icons.account_box, "Account People"),
+              SizedBox(
+                height: 10,
+              ),
+              buildCard(Icons.adb_outlined, "Android"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Card buildCard(IconData iconData, String text) {
+  return Card(
+    elevation: 5,
+    child: Row(
+      children: [
+        Container(
+          height: 80,
+          margin: EdgeInsets.all(5),
+          padding: EdgeInsets.only(left: 20),
+          child: Icon(
+            iconData,
+            color: Colors.green[400],
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Text(text)
+      ],
+    ),
+  );
+}
