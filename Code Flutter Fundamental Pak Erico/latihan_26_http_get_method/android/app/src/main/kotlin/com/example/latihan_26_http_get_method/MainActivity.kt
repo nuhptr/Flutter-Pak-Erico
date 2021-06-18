84776c40cb7dd69749fcef14404e454f870001c7
nuhptr <nugrahaadi733@gmail.com>
2021-06-19 02:06:17 +0700

    delete

--- Code Flutter Fundamental Pak Erico/latihan_26_http_get_method/android/app/src/main/kotlin/com/example/latihan_26_http_get_method/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_26_http_get_method
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Code Flutter Fundamental Pak Erico/latihan_27_http_data_banyak/android/app/src/main/kotlin/com/example/latihan_27_http_data_banyak/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_27_http_data_banyak
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
