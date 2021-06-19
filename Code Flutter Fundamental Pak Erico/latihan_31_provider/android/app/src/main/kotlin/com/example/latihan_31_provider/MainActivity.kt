4ec1c28dbf2d6d9c4795b5f3c3f397c243fcdb79
nuhptr <nugrahaadi733@gmail.com>
2021-06-19 13:22:15 +0700

    delete

--- Code Flutter Fundamental Pak Erico/latihan_31_provider/android/app/src/main/kotlin/com/example/latihan_31_provider/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_31_provider
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Code Flutter Fundamental Pak Erico/latihan_49_division_css/android/app/src/main/kotlin/com/example/latihan_49_division_css/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.latihan_49_division_css
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
