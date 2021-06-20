a08c4f760207dad95d1b62217a1208c52247c31f
nuhptr <nugrahaadi733@gmail.com>
2021-06-20 10:02:16 +0700

    update colorBloc

--- latihan33_bloc/lib/color_bloc.dart
+++ latihan33_bloc/lib/color_bloc.dart
@@ -2,10 +2,16 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-// TODO: untuk membuat eventnya
-enum ColorEvent { to_yellow, to_blue }
+// TODO: class enum untuk membuat eventnya
+enum ColorEvent { to_green, to_blue }
 
 class ColorBloc {
+  // TODO: membaca inputan yang masuk
+  ColorBloc() {
+    _event!.stream.listen(_mapEventToState);
+  }
+
+  // TODO: properties kelas untuk inisialisasi
   Color? _color = Colors.yellow[300];
 
   // TODO: controller untuk event dan pembuka eventnya
@@ -13,5 +19,24 @@ class ColorBloc {
   StreamSink<ColorEvent> get eventSink => _event!.sink;
 
   // TODO: controller untuk state dan pembuka statenya
-  
+  StreamController<Color>? _state = StreamController<Color>();
+  StreamSink<Color> get _stateSink => _state!.sink; //* masih di dalam selang
+  Stream<Color> get stateStream => _state!.stream; //* sudah menjadi output
+
+  // TODO: untuk mapping event yang masuk dan output yang dihasilkan
+  void _mapEventToState(ColorEvent colorEvent) {
+    if (colorEvent == ColorEvent.to_green) {
+      _color = Colors.green[300];
+    } else {
+      _color = Colors.blue[300];
+    }
+
+    _stateSink.add(_color!); //* memasukan ke dalam selang
+  }
+
+  // TODO: untuk membereskan selang state
+  void dispose() {
+    _event!.close();
+    _state!.close();
+  }
 }
--- latihan33_bloc/lib/main.dart
+++ latihan33_bloc/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:latihan33_bloc/color_bloc.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
@@ -15,9 +17,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // TODO: properties kelas home
+  ColorBloc? colorBloc = ColorBloc();
+
+  // TODO: untuk menutup state yang terjadi sehingga memori tidak menumpuk
+  @override
+  void dispose() {
+    colorBloc!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        // TODO: streambuilder digunakan untuk BLOC
+        child: StreamBuilder<Color>(
+          stream: colorBloc!.stateStream,
+          initialData: Colors.green[300],
+          builder: (BuildContext context, snapshot) => AnimatedContainer(
+            decoration: BoxDecoration(
+              color: snapshot.data,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            duration: const Duration(milliseconds: 500),
+            width: 100,
+            height: 100,
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            elevation: 0,
+            onPressed: () {
+              // TODO: memasukan event to_yellow ke _mapEventToState
+              colorBloc!.eventSink.add(ColorEvent.to_green);
+            },
+            backgroundColor: Colors.green[300],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            elevation: 0,
+            onPressed: () {
+              // TODO: memasukan event to_blue ke _mapEventToState
+              colorBloc!.eventSink.add(ColorEvent.to_blue);
+            },
+            backgroundColor: Colors.blue[300],
+          ),
+        ],
+      ),
+    );
   }
 }
